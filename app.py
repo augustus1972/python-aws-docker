f466d68017ab5b8096a4a6a1c86f3d9c037e7b45
augustus1972 <49292803+augustus1972@users.noreply.github.com>
2026-07-05 22:18:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import socket
+import platform
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     hostname = socket.gethostname()
-    return f"Witaj w mojej aplikacji Python! Serwer produkcyjny: {hostname}"
+    python_version = platform.python_version()
+    return f"witaj w mojej aplikacji python (Python {python_version}) tj. Artura Kotarskiego zawieszonej w chmurze AWS, Serwer produkcyjny: {hostname}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
